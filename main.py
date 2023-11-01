15942975c8c01ed9d7d167602485f9a22e7d233f
Claudio_Casellato <claudiocasellatoml@gmail.com>
2023-11-01 10:33:05 +0100

    main update

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ mix = Mixture(cat=Categorical(logits=[1, 1]),
 
 x = mix.sample(3000)
 
-# Use Direct Gradi
+# Use Direct Gradient estimation
 model = models.SlicedScoreMatching(hidden_layers=(512, 512))
 model.compile("adam")
 model.fit(x, epochs=1000, batch_size=3000)
@@ -23,7 +23,7 @@ fig, ax = plt.subplots(2)
 fig, ax0 = utils.make_distribution_grad_plot(mix, fig=fig, ax=ax[0])
 ax0.scatter(x[:, 0], x[:, 1], color="black", s=5)
 ax1 = utils.make_grad_plot(model, ax=ax[1])
-
+fig.tight_layout()
 
 # Use Energy Based Model
 ebm = models.EBMSlicedScoreMatching(hidden_layers=(512, 512))
@@ -34,3 +34,4 @@ fig, ax = plt.subplots(2, figsize=(10, 20))
 fig, ax0 = utils.make_distribution_grad_plot(mix, fig=fig, ax=ax[0])
 ax0.scatter(x[:, 0], x[:, 1], color="black", s=5)
 ax1 = utils.make_grad_plot(ebm, ax=ax[1], fig=fig)
+fig.tight_layout()
